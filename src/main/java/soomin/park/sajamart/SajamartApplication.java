a931143d68f8428ccab065fe0c53686d5db44710
soomin.park <parksoopark@gmail.com>
2024-06-17 22:46:39 +0900

    최초커밋

---
+++ src/main/java/soomin/park/sajamart/SajamartApplication.java
@@ -0,0 +1,13 @@
+package soomin.park.sajamart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SajamartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SajamartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/soomin/park/sajamart/SajamartApplicationTests.java
@@ -0,0 +1,13 @@
+package soomin.park.sajamart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SajamartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
